c2570d42cf15dff9f13b911047f1dae87ca56fbd
Anne Hu <anne.hu@tufts.edu>
2024-10-12 09:49:32 -0400

    Async and class update -  picoConductor.py
    Incorporated a Conductor class, started implementing asyncio

--- picoConductor.py
+++ picoConductor.py
@@ -6,11 +6,19 @@ from mqtt import MQTTClient
 from machine import Pin, PWM # not in use yet...
 from secrets import mysecret, key
 
+# ------ CREATING A CONDUCTOR ------
+# Pico should connect before handilng MQTT requests because this requires manual input from the Mac user
+midi = Yell('frog', verbose = True, type = 'midi')
+
+from Conductor import Conductor # Class to control the tune
+conductor = Conductor(midi)
+conductor.connect()
 
 # ------ MQTT SET UP ------
 mqtt_broker = 'broker.hivemq.com'
 port = 1883
 topic_sub = 'ME35-24/linuslucy' # goal is to play linus and lucy!
+isOn = False # tracking MQTT commands
 
 def connect_wifi():
     wlan = network.WLAN(network.STA_IF)
@@ -26,8 +34,20 @@ def connect_mqtt(client):
     print(f'Subscribed to {topic_sub}')
     
 def callback(topic, msg):
+    global conductor
+    
     val = msg.decode()
     print("MQTT Message received: "+val)
+    
+    if val == 'start':
+        conductor.turnOn()
+        print("CALLBACK - STARTING")
+    elif val == 'stop':
+        conductor.turn.Off()
+        print("CALLBACK - STOPPING")
+    elif val == 'bye':
+        conductor.disconnect()
+        print("CALLBACK - DISCONNECTING")
     
 async def mqtt_handler(client):
     while True:
@@ -42,35 +62,38 @@ async def mqtt_handler(client):
             connect_wifi()
         await asyncio.sleep(0.01)
 
-# ------ MIDI SET UP ------
-NoteOn = 0x90
-NoteOff = 0x80
-StopNotes = 123
-SetInstroment = 0xC0
-Reset = 0xFF
-
-velocity = {'off':0, 'pppp':8,'ppp':20,'pp':31,'p':42,'mp':53,
-    'mf':64,'f':80,'ff':96,'fff':112,'ffff':127}
-
-# ------ CONNECTING UP BLUETOOTH ------
-connect_wifi()
-
-p = Yell('frog', verbose = True, type = 'midi')
-p.connect_up()
+# ------ MAIN CODE ------
+async def main():
+    global isOn
+    global midi
+    
+    
+    
+    while True:
+        if isOn:
+            print("----- IN NOTE LOOP ----- ")
+            for i in range(5):
+                midi.send(payload)
+            asyncio.sleep(5)
+        else:
+            print("--- Waiting for start command ---")
+        await asyncio.sleep(0.01)
         
-channel = 0
-note = 55
-cmd = NoteOn
+        
 
-channel = 0x0F & channel
-timestamp_ms = time.ticks_ms()
-tsM = (timestamp_ms >> 7 & 0b111111) | 0x80
-tsL =  0x80 | (timestamp_ms & 0b1111111)
+# ------ CONNECTING UP MQTT -------
+# After midi bluetooth keyboard is set up, start handling MQTT 
+connect_wifi()
+client = MQTTClient('ME35_linuslucy', mqtt_broker, port, keepalive=60)
+client.set_callback(callback)
+client.connect()
+client.subscribe(topic_sub.encode())
+print(f'Subscribed to {topic_sub}')
 
-c =  cmd | channel     
-payload = bytes([tsM,tsL,c,note,velocity['f']])
+# ----- RUNNING ASYNC FUNCTIONS -----
+loop = asyncio.get_event_loop()
+loop.create_task(mqtt_handler(client))
+loop.create_task(conductor.testTune())
+#loop.create_task(main())
+loop.run_forever()
 
-for i in range(5):
-    p.send(payload)
-    time.sleep(5)
-p.disconnect()
