facaa6f16002221fdb0e8a4d1350ad945e2a73c8
Anne Hu <anne.hu@tufts.edu>
2024-10-19 17:14:33 -0400

    Update dahalLinusLucy.py
    Now the potentiometer values are being read correctly!

--- dahalLinusLucy.py
+++ dahalLinusLucy.py
@@ -8,13 +8,40 @@ import ssd1306
 import adxl345
 
 from secrets import mysecret, key
-    
-# ----- BOARD SETUP  ------
-i2c = SoftI2C(scl = Pin(7), sda = Pin(6))
 
+# ------ BOARD FUNCTIONS -----
+i2c = SoftI2C(scl = Pin(7), sda = Pin(6))
 screen = ssd1306.SSD1306_I2C(128,64,i2c)
 screen.text('Hello World', 0, 0, 1) # to display text
 screen.show()
+
+# Prints a set message (for now) to the LCD onboard
+def printMessage():
+    global screen
+    screen.fill(0)
+    screen.text('Good Grief ._.', 0, 0, 1) # to display text
+    screen.show()
+
+# async function to read potentiometer values and send via MQTT
+async def readPot(client):
+    global screen
+    
+    # Potentiometer range = 1 -> 4095
+    # tempo will range from pot(1) = 1 to pot(4095) = 3
+    potMax = 4095 # maximum value read by potentiometer
+    pot = ADC(Pin(3))
+    pot.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
+
+    while True:
+        val = pot.read()
+        tempo = 1 + 2*(val/potMax)
+        client.publish(topic_pub, 'T'+str(tempo))
+        
+        screen.fill(0)
+        screen.text("Published: "+str(tempo), 0, 0, 1)
+        screen.show()
+        await asyncio.sleep(0.1) #will be refreshing more slowly than other things for now
+        
 
 # ------ MQTT SET UP ------
 mqtt_broker = 'broker.hivemq.com'
@@ -29,47 +56,21 @@ def connect_wifi():
     while not wlan.isconnected():
         time.sleep(1)
     print('----- connected to wifi -----')
-    
+
+# Helper function to be called if the client somehow gets disconnected
 def connect_mqtt(client):
     client.connect()
     client.subscribe(topic_sub.encode())
     print(f'Subscribed to {topic_sub}')
 
-def printMessage():
-    global screen
-    screen.fill(0)
-    screen.text('Good Grief ._.', 0, 0, 1) # to display text
-    screen.show()
-    
-def callback(topic, msg):
-    global conductor
-    
+# Handling MQTT subscriptions
+def callback(topic, msg):    
     val = msg.decode()
     print("MQTT Message received: "+val)
 
     if val == 'msg':
         printMessage()
-        
-async def readPot(client):
-    global screen
-    
-    # Potentiometer range = 1 -> 4095
-    # tempo will range from pot(1) = 1 to pot(4095) = 3
-    potMax = 4095 # maximum value read by potentiometer
-    pot = ADC(Pin(3))
-    pot.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
 
-    while True:
-        val = pot.read()
-        tempo = 1 + 2*(potMax-val)
-        client.publish(topic_pub, 'T'+str(tempo))
-        
-        screen.fill(0)
-        screen.text("Published multiplier: "+str(tempo), 0, 0, 1)
-        screen.show()
-        await asyncio.sleep(0.01)
-        
-    
 async def mqtt_handler(client):
     while True:
         if network.WLAN(network.STA_IF).isconnected():
@@ -89,7 +90,7 @@ connect_wifi()
 client = MQTTClient('linusLucyDahal', mqtt_broker, port, keepalive=60)
 client.set_callback(callback)
 client.connect()
-#client.subscribe(topic_sub.encode())
+client.subscribe(topic_sub.encode())
 print(f'Subscribed to {topic_sub}')
 
 # ----- RUNNING ASYNC FUNCTIONS -----
@@ -97,3 +98,4 @@ loop = asyncio.get_event_loop()
 loop.create_task(mqtt_handler(client))
 loop.create_task(readPot(client))
 loop.run_forever()
+
