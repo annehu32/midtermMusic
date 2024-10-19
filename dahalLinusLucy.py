cbc0200804fe02449dc247689bfc02e6f515e567
Anne Hu <anne.hu@tufts.edu>
2024-10-19 16:53:24 -0400

    Update dahalLinusLucy.py
    Updated potentiometer function to MQTT a tempo multiplier, but MQTT is working unreliably

--- dahalLinusLucy.py
+++ dahalLinusLucy.py
@@ -9,31 +9,18 @@ import adxl345
 
 from secrets import mysecret, key
     
-# ----- TEST CODE ------
+# ----- BOARD SETUP  ------
 i2c = SoftI2C(scl = Pin(7), sda = Pin(6))
 
 screen = ssd1306.SSD1306_I2C(128,64,i2c)
 screen.text('Hello World', 0, 0, 1) # to display text
 screen.show()
 
-adx = adxl345.ADXL345(i2c)
-#print(adx.xValue)
-
-pot = ADC(Pin(3))
-pot.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
-#print(pot.read()) #range 0-4095
-
-motor = servo.Servo(Pin(2)) 
-#motor.write_angle(90) # - to turn the motor to 30 degrees
-
-sd = Pin(8, Pin.IN)
-sd.value()
-
-
-# ------ MQTT SUBSCRIBER SET UP ------
+# ------ MQTT SET UP ------
 mqtt_broker = 'broker.hivemq.com'
 port = 1883
 topic_sub = 'ME35-24/linuslucy'
+topic_pub = 'ME35-24/linuslucy'
 
 def connect_wifi():
     wlan = network.WLAN(network.STA_IF)
@@ -48,9 +35,8 @@ def connect_mqtt(client):
     client.subscribe(topic_sub.encode())
     print(f'Subscribed to {topic_sub}')
 
-def sendMessage():
+def printMessage():
     global screen
-    print("inside sendMessage()")
     screen.fill(0)
     screen.text('Good Grief ._.', 0, 0, 1) # to display text
     screen.show()
@@ -62,8 +48,26 @@ def callback(topic, msg):
     print("MQTT Message received: "+val)
 
     if val == 'msg':
-        sendMessage()
+        printMessage()
+        
+async def readPot(client):
+    global screen
+    
+    # Potentiometer range = 1 -> 4095
+    # tempo will range from pot(1) = 1 to pot(4095) = 3
+    potMax = 4095 # maximum value read by potentiometer
+    pot = ADC(Pin(3))
+    pot.atten(ADC.ATTN_11DB) # the pin expects a voltage range up to 3.3V
 
+    while True:
+        val = pot.read()
+        tempo = 1 + 2*(potMax-val)
+        client.publish(topic_pub, 'T'+str(tempo))
+        
+        screen.fill(0)
+        screen.text("Published multiplier: "+str(tempo), 0, 0, 1)
+        screen.show()
+        await asyncio.sleep(0.01)
         
     
 async def mqtt_handler(client):
@@ -82,14 +86,14 @@ async def mqtt_handler(client):
 # ------ CONNECTING UP MQTT -------
 # After midi bluetooth keyboard is set up, start handling MQTT 
 connect_wifi()
-client = MQTTClient('ME35_linuslucy2', mqtt_broker, port, keepalive=60)
+client = MQTTClient('linusLucyDahal', mqtt_broker, port, keepalive=60)
 client.set_callback(callback)
 client.connect()
-client.subscribe(topic_sub.encode())
+#client.subscribe(topic_sub.encode())
 print(f'Subscribed to {topic_sub}')
 
 # ----- RUNNING ASYNC FUNCTIONS -----
 loop = asyncio.get_event_loop()
 loop.create_task(mqtt_handler(client))
+loop.create_task(readPot(client))
 loop.run_forever()
-
