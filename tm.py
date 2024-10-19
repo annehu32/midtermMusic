6f54217b3c34aa16fb866bcaf7c34902724bde08
Anne Hu <anne.hu@tufts.edu>
2024-10-19 15:39:59 -0400

    Update tm.py
    Now reads information for 3 classes and the "nothing class", sends out MQTT to ME35-24/linuslucy topic.

--- tm.py
+++ tm.py
@@ -4,11 +4,7 @@ from pyscript.js_modules import teach, pose, ble_library, mqtt_library
 #ble = ble_library.newBLE()
 myClient = mqtt_library.myClient
 mqtt_connected = False
-sub_topic = 'ME35-24/linuslucy'
 pub_topic = 'ME35-24/linuslucy'
-
-async def received_mqtt_msg(message):
-    message = myClient.read().split('	')  #add here anything you want to do with received messages
 
 async def run_model(URL2):
     s = teach.s  # or s = pose.s
@@ -20,22 +16,11 @@ async def connect(name):
     myClient.init()
     while not myClient.connected:
         await asyncio.sleep(2)
-    myClient.subscribe(sub_topic)
-    myClient.callback = received_mqtt_msg
     mqtt_connected = True
-    #if await ble.ask(name):
-    #    print('name ',name)
-    #    await ble.connect() 
-    #    print('connected!')
-        
-async def disconnect():
-    #await ble.disconnect()
-    print('disconnected')
 
 def send(message):
     print('sending ', message)
     myClient.publish(pub_topic, message)
-    #ble.write(message)
 
 def get_predictions(num_classes):
     predictions = []
@@ -44,14 +29,39 @@ def get_predictions(num_classes):
         if divElement:
             divValue = divElement.innerHTML
             predictions.append(divValue)
+            print(divValue)
     return predictions
 
 import asyncio
 await run_model("https://teachablemachine.withgoogle.com/models/t58WGMjvv/") #Change to your model link
 await connect('Anne')
 
+# Handling message sending
+threshold = 0.9 
+lastMessage = None
+
 while True:
     if mqtt_connected:
-        predictions = get_predictions(2)
-        send(str(predictions))
+        predictions = get_predictions(4)   
+
+        for guess in predictions:
+            if guess[:4] == 'lucy':
+                print("READING LUCY!!!")
+                confidence = float(guess[6:])
+                if confidence > threshold and not lastMessage == 'LU':
+                    send('LU')
+                    lastMessage = 'LU'
+            elif guess[:5] =='linus' and not lastMessage == 'LI':
+                print("READING LINUS!!!")
+                confidence = float(guess[7:])
+                if confidence > threshold:
+                    send('LI')
+                    lastMessage = 'LI'
+            elif guess[:6] == 'snoopy' and not lastMessage == 'SN':
+                print("READING SNOOPY!!!")
+                confidence = float(guess[8:])
+                if confidence > threshold:
+                    send('SN')
+                    lastMessage = 'SN'
+        
     await asyncio.sleep(2)
