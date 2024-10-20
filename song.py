86f60324521597ded77359fda11ff6da681c9836
Anne Hu <anne.hu@tufts.edu>
2024-10-20 12:21:01 -0400

    Create song.py
    Just a song :)

---
+++ song.py
@@ -0,0 +1,36 @@
+# For Midterm project to help clean up the main code
+# Notes:
+# 64 = quarter note
+# 32 = eight note
+
+melody = [
+    (48, 32), # measure 1
+    (50, 32),
+    (52, 64),
+    (52, 32),
+    (50, 32),
+    (48, 64),
+    (50, 96),# measure 2
+    (48, 160),
+    (48, 32), #measure 3
+    (50, 32),
+    (52, 64),
+    (52, 384), # measure 3+4
+    (48, 32), # measure 5
+    (50, 32),
+    (52, 64),
+    (52, 32),
+    (50, 32),
+    (48, 64),
+    (50, 96), # measure 6
+    (48, 160),
+    (48, 96), # measure 7
+    (50, 32),
+    (50, 384), # measure 7 + 8
+    (48, 96), # measure 9
+    (50, 32),
+    (50, 384), # measure 10 + 11
+    (79, 64),
+    (79, 64)
+]
+
