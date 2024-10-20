69568e66bb1266cffa7b51d77b53fac5ac4bf7bc
Anne Hu <anne.hu@tufts.edu>
2024-10-20 12:21:38 -0400

    Update Conductor.py
    Notes have been moved to separate file, tempo changes.

--- Conductor.py
+++ Conductor.py
@@ -14,7 +14,7 @@ class Conductor():
         self.isOn = False
         self.pauseIndex = 0 # For pausing function, want to know at what note were we paused
         print("----- conductor successfully instantiated------")
-        self.isPlaying = False
+        self.tempo = 2.0
     
     def turnOn(self):
         self.isOn = True
@@ -24,8 +24,14 @@ class Conductor():
         self.isOn = False
         print(" ~~~~~~ from conductor: turnOff() ~~~~~")
     
-    def startPlay(self):
-        self.isPlaying = True
+    async def changeTempo(self, val):
+        self.tempo = float(val)
+        print("-------- TEMPO CHANGED!!!!! -------")
+        print(" ------- " + str(self.tempo)+ " ----------")
+        await asyncio.sleep(0.01)
+    
+    def getTempo(self):
+        return self.tempo
 
     def connect(self):
         self.midi.connect_up()
@@ -36,55 +42,7 @@ class Conductor():
     def checkState(self):
         return self.isOn
         
-    def playTune(self):
-        print(" ~~~~~~ from conductor: playTune() ~~~~~")
-        # ----- MIDI SETUP ------
-        NoteOn = 0x90
-        NoteOff = 0x80
-        StopNotes = 123
-        SetInstroment = 0xC0
-        Reset = 0xFF
-
-        velocity = {'off':0, 'pppp':8,'ppp':20,'pp':31,'p':42,'mp':53,
-            'mf':64,'f':80,'ff':96,'fff':112,'ffff':127}
-            
-        channel = 0
-        note = 55
-        cmd = NoteOn
-
-        channel = 0x0F & channel
-        timestamp_ms = time.ticks_ms()
-        tsM = (timestamp_ms >> 7 & 0b111111) | 0x80
-        tsL =  0x80 | (timestamp_ms & 0b1111111)
-        
-        c =  cmd | channel     
-        payload = bytes([tsM,tsL,c,note,velocity['f']])
-
-        
-        # Defining the tune!!!!
-        notesRightHand = [
-            (74, 64),
-            (79,64)
-        ]
-        
-        notesLeftHand = [
-            (38, 64),
-            (36, 64),
-            (38, 32),
-            (36, 32)
-        ]
-        
-        
-        # ------- Playing the tune -----
-        for note, duration in notesLeftHand:
-            # note on
-            payload = bytes([tsM,tsL,c,note,velocity['f']])
-            self.midi.send(payload)
-            time.sleep(duration/1000)
-            
-        
     async def handler(self):
-        print(" ~~~~~~ from conductor: handler() ~~~~~")
         # ----- MIDI SETUP ------
         NoteOn = 0x90
         NoteOff = 0x80
@@ -94,11 +52,11 @@ class Conductor():
 
         velocity = {'off':0, 'pppp':8,'ppp':20,'pp':31,'p':42,'mp':53,
             'mf':64,'f':80,'ff':96,'fff':112,'ffff':127}
-            
+        
         channel = 0
         note = 55
         cmd = NoteOn
-
+        
         channel = 0x0F & channel
         timestamp_ms = time.ticks_ms()
         tsM = (timestamp_ms >> 7 & 0b111111) | 0x80
@@ -106,62 +64,29 @@ class Conductor():
 
         c =  cmd | channel     
         payload = bytes([tsM,tsL,c,note,velocity['f']])
-    
-        # Notes:
-        # 64 = quarter note
-        # 32 = eight note
-        # am working in Octave 2 and Octave 3
-        melody = [
-            (48, 32), # measure 1
-            (50, 32),
-            (52, 64),
-            (52, 32),
-            (50, 32),
-            (48, 64),
-            (50, 96),# measure 2
-            (48, 160),
-            (48, 32), #measure 3
-            (50, 32),
-            (52, 64),
-            (52, 384), # measure 3+4
-            (48, 32), # measure 5
-            (50, 32),
-            (52, 64),
-            (52, 32),
-            (50, 32),
-            (48, 64),
-            (50, 96), # measure 6
-            (48, 160),
-            (48, 96), # measure 7
-            (50, 32),
-            (50, 384), # measure 7 + 8
-            (48, 96), # measure 9
-            (50, 32),
-            (50, 384), # measure 10 + 11
-            (79, 64),
-            (79, 64)
-        ]
-
-# next step: incorporate a pause with a flag somehow
+        from song import melody
+     
         while True:
-            # start notes
+            print("************ outside song loop, tempo is: "+str(self.tempo))
+            # For each note in song, make sure light uncovered, then play
             for i in range(0, len(melody)):
+                temp = self.getTempo()
+                print("----- tempo is: "+str(temp)+" --------")
+
                 print("----- Note index: "+str(i)+" --------")
                 note = melody[i]
                 
                 while not self.isOn:
-                    #print(" ------- Waiting for go on note #: "+str(i)+" --------")
+                    print(" ------- Waiting for go on note #: "+str(i)+" --------")
                     await asyncio.sleep(0.01)
                 
                 # When isOn, will play the note
                 payload = bytes([tsM,tsL,c,note[0],velocity['f']])
                 self.midi.send(payload)
                 
-                await asyncio.sleep(note[1]/100)
+                await asyncio.sleep(note[1]/(temp*100))
                 
                 payloadOff = bytes([tsM,tsL,c,note[0],0])
                 self.midi.send(payloadOff)
                     
-
             await asyncio.sleep(0.01)
-    
